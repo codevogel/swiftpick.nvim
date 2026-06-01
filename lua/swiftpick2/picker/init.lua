c0b8b676d0143fa2fa70d5eacce7d6ff39681c7f
Kamiel de Visser <kamieldevisser@gmail.com>
2026-06-01 22:04:05 +0200

    adjust window sizing

--- lua/swiftpick2/picker/init.lua
+++ lua/swiftpick2/picker/init.lua
@@ -4,9 +4,9 @@ local M = {
 }
 
 function M.open_picker()
-  local entry_buf = vim.api.nvim_create_buf(false, false)
-  vim.api.nvim_buf_set_lines(entry_buf, 0, -1, false, { "Hello from SwiftPick2!" })
-  vim.api.nvim_open_win(entry_buf, false, M.window.get_centered_win_config(vim.api.nvim_buf_line_count(entry_buf)))
+  local entry_buf_nr = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(entry_buf_nr, 0, -1, false, { "Hello from SwiftPick2!" })
+  vim.api.nvim_open_win(entry_buf_nr, false, M.window.get_centered_win_config(entry_buf_nr))
 end
 
 return M
--- lua/swiftpick2/picker/window.lua
+++ lua/swiftpick2/picker/window.lua
@@ -1,30 +1,45 @@
 local M = {}
 
-local function get_window_size()
+local function get_window_size(buf_size)
+  local win_size = {
+    width = vim.fn.min({ buf_size.width + 2, vim.o.columns - 4 }),
+    height = vim.fn.min({ buf_size.height + 2, vim.o.lines - 4 }),
+  }
+  return win_size
+end
+
+local function get_buf_size(entry_buf_nr)
+  local line_count = vim.api.nvim_buf_line_count(entry_buf_nr)
+
+  local max_line_length = 0
+  for i = 1, line_count do
+    local line_length = #vim.api.nvim_buf_get_lines(entry_buf_nr, i - 1, i, false)[1]
+    if line_length > max_line_length then
+      max_line_length = line_length
+    end
+  end
+
   return {
-    width = vim.o.columns,
-    height = vim.o.lines,
+    width = max_line_length,
+    height = line_count,
   }
 end
 
-function M.get_centered_win_config(buf_line_count)
-  local size = get_window_size()
-  local row = math.floor((vim.o.lines - size.height) / 2)
-  local col = math.floor((vim.o.columns - size.width) / 2)
+function M.get_centered_win_config(entry_buf_nr)
+  local win_size = get_window_size(get_buf_size(entry_buf_nr))
+  local row = math.floor((vim.o.lines - win_size.height) / 2)
+  local col = math.floor((vim.o.columns - win_size.width) / 2)
   return {
     relative = "editor",
     row = row,
     col = col,
-    width = size.width,
-    height = size.height,
+    width = win_size.width,
+    height = win_size.height,
     border = "rounded",
     style = "minimal",
+    title = "swiftpick",
+    title_pos = "center",
   }
-end
-
-function M.refresh_window_config(win_id)
-  local config = get_centered_win_config()
-  vim.api.nvim_win_set_config(win_id, config)
 end
 
 return M
