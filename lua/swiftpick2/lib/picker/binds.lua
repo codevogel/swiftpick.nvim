18a775acc2c136f89d7bba0d39fb2c8aa3439aac
Kamiel de Visser <kamieldevisser@gmail.com>
2026-06-04 12:21:17 +0200

    read close binds from config

--- lua/swiftpick2/lib/picker/binds.lua
+++ lua/swiftpick2/lib/picker/binds.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local config = require("swiftpick2.config")
+
 local function create_local_buffer_keybind(buf, mode, key, callback)
   vim.keymap.set(mode, key, callback, { buffer = buf, noremap = true, silent = true })
 end
@@ -8,10 +10,10 @@ function M.create_picker_keybinds(win, buf)
   if win == nil then
     error("Picker window number is nil. Cannot create keybinds.")
   end
-  create_local_buffer_keybind(buf, "n", "<Esc>", function()
+  create_local_buffer_keybind(buf, "n", config.values.keybinds.close_picker, function()
     vim.api.nvim_win_close(win, true)
   end)
-  create_local_buffer_keybind(buf, "n", "<C-c>", function()
+  create_local_buffer_keybind(buf, "n", config.values.keybinds.close_picker_alt, function()
     vim.api.nvim_win_close(win, true)
   end)
 end
