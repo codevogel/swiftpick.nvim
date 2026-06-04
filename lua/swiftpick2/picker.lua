504542e079012a70f060adc10a32520fe38a63d0
Kamiel de Visser <kamieldevisser@gmail.com>
2026-06-04 14:01:01 +0200

    don't open picker when it's already open

--- lua/swiftpick2/picker.lua
+++ lua/swiftpick2/picker.lua
@@ -4,6 +4,9 @@ local window = require("swiftpick2.lib.picker.window")
 local state = require("swiftpick2.state")
 
 function M.open_picker()
+  if state.opened then
+    return
+  end
   state.opened_from_window = vim.api.nvim_get_current_win()
   state.opened_from_buffer = vim.api.nvim_get_current_buf()
   window.create_picker_window(M.on_close_picker)
@@ -12,6 +15,7 @@ end
 function M.on_close_picker()
   state.opened_from_window = nil
   state.opened_from_buffer = nil
+  state.opened = false
 end
 
 return M
--- lua/swiftpick2/state.lua
+++ lua/swiftpick2/state.lua
@@ -1,8 +1,10 @@
 local M = {}
 
 M.state = {
+  opened = false,
   opened_from_window = nil,
   opened_from_buffer = nil,
+  pending_at_action = nil,
 }
 
 return M
